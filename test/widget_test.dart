dbfcda51700bd6c5adcb37b7c60319cf6d7315c0
Steven Mak <stevenmak@gmail.com>
2020-11-11 17:02:56 +0800

    remove unused boilerplate test code and add a new test to select an item

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:boringshop/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('select an item and put in into shopping cart', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byKey(Key('item1')), findsOneWidget);
   });
 }
